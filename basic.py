b783d504216ef3170bbdb2d3c7089ea80e3075cc
tjrwnsdl1010 <aodxo1010@naver.com>
2023-06-09 09:54:55 +0900

    start python

---
+++ basic.py
@@ -0,0 +1,20 @@
+# 1 대소문자로 데이터를 구분 apple != Apple
+# 2 명령어의 띄어쓰기 주의
+# 3 message / massage 오타 조심
+
+
+# 변수 variable
+dust = 10
+dust = '10'
+greeting = 'hello'
+
+# 1
+status = True
+# 0
+status = False
+
+print(dust, greeting, status)
+
+# 배열
+dust_list = [10, 20, 20, 15, 100, 150]
+print(dust_list)
