2d6925eaad84a8a915e250393bc6523a0787e862
tjrwnsdl1010 <aodxo1010@naver.com>
2023-06-09 10:41:55 +0900

    save

--- basic.py
+++ basic.py
@@ -15,6 +15,33 @@ status = False
 
 print(dust, greeting, status)
 
-# 배열
+# List, 배열
 dust_list = [10, 20, 20, 15, 100, 150]
-print(dust_list)
+print(dust_list[1])
+
+# Dictionary
+dust_dict = {
+    '서울': 100,
+    '대전': 50,
+    '부산': 10,
+}
+
+print(dust_dict['서울'])
+
+# 조건문
+age = 10
+
+if age > 20: 
+    print('성인입니다.')
+elif age > 8:
+    print("청소년입니다.")
+else:
+    print('어린이입니다.')
+
+# 반복문
+menus = ['짜장면', '국밥', '김밥', '라면', '피자']
+
+n = 0
+while n < 4:
+    print(menus[n])
+    n += 1
